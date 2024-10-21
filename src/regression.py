35b344af429892e9358f690e6407c59ab73e876c
adityapentyala <adityapentyala02@gmail.com>
2024-10-21 23:06:56 +0530

    fix: typo

--- src/regression.py
+++ src/regression.py
@@ -9,7 +9,7 @@ def create_regression_data_OLS(df, drop=[], dummies=False, ratios=False):
     for col in drop:
         X=X.drop(columns=[col])
     X = add_constant(X)
-    y = df["gdp growth rate"]
+    y = df["tradeflow_baci"]
     return X, y
 
 def fit_OLS_model(X, y):
