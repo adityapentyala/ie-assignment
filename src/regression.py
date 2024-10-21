8491c73ad51c1260d70a6e653f909ac17a371bc5
adityapentyala <adityapentyala02@gmail.com>
2024-10-21 23:19:05 +0530

    feat: add data dir and analysis notebook

--- src/regression.py
+++ src/regression.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 from statsmodels.api import OLS, add_constant
 
-def create_regression_data_OLS(df, drop=[], dummies=False, ratios=False):
+def create_regression_data_OLS(df, drop=[]):
     X = df[[
 
     ]]
