ec2c7715608adc7738f55a23242bf9b483c77f03
adityapentyala <adityapentyala02@gmail.com>
2024-10-22 11:17:53 +0530

    idk bro idr anymore

---
+++ src/plots.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+def plot_reg_lines(raw_df, X, y, model):
+    
+        
--- src/regression.py
+++ src/regression.py
@@ -9,7 +9,7 @@ def create_regression_data_OLS(df, drop=[]):
     for col in drop:
         X=X.drop(columns=[col])
     X = add_constant(X)
-    y = df["tradeflow_baci"]
+    y = df["mean_trade"]
     return X, y
 
 def fit_OLS_model(X, y):
