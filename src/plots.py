11b19365d1a0fd01bde7eb26030ede1773b616fe
adityapentyala <adityapentyala02@gmail.com>
2024-10-22 11:52:37 +0530

    feat: re-run notebook with plots

--- src/plots.py
+++ src/plots.py
@@ -1,7 +1,21 @@
 import pandas as pd
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plot
 import numpy as np
 
-def plot_reg_lines(raw_df, X, y, model):
-    
-        
+def plot_reg_lines(df, model, cols, target):
+    means = {}
+    mean_y = 0
+    for col in cols:
+        means[col] = np.mean(df[col])
+        mean_y+=model.params[col]*means[col]
+    for col in cols:
+        plot.figure()
+        pred_y = model.params[col]*df[col] + model.params["const"] + mean_y - model.params[col]*means[col]
+        plot.plot(df[col], pred_y, color="red", label="predictions")
+        plot.scatter(df[col], pred_y, color="red")
+        plot.scatter(df[col], df[target])
+        plot.title(f"Fitted ln({target}) vs ln({col})")
+        plot.xlabel(f"ln({col})")
+        plot.ylabel(f"ln({target})")
+        plot.legend()
+        plot.show()
--- src/regression.py
+++ src/regression.py
@@ -2,14 +2,12 @@ import pandas as pd
 import numpy as np
 from statsmodels.api import OLS, add_constant
 
-def create_regression_data_OLS(df, drop=[]):
-    X = df[[
-        'gdp_o', 'gdp_d', 'dist', 'NRE_India','NRE_x', 'LSC_India', 'LSC_x', 'Corruption_India', 'Corruption_X'
-    ]]
+def create_regression_data_OLS(df, cols, target, drop=[]):
+    X = df[cols]
     for col in drop:
         X=X.drop(columns=[col])
     X = add_constant(X)
-    y = df["mean_trade"]
+    y = df[target]
     return X, y
 
 def fit_OLS_model(X, y):
